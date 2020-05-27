7bdca0d74761fcce153dbdaa3f9fc91706724779
Lomcarr91 <56019533+Lomcarr91@users.noreply.github.com>
2020-05-27 10:50:43 +0200

    ok

---
+++ temaoppgaven/src/App.svelte
@@ -0,0 +1,64 @@
+<script>
+	//Dette er fra det gamle app.svelte dokumentet
+	let addInfo
+
+	addInfo.addEventListner('click')
+
+</script>
+
+
+
+<main>
+	<h3>Call To Mind</h3>
+
+	<img src="public/bilder/logo3.svg" alt="">
+
+	<div id="intro">
+		<h4>Du har ingen planlagte anrop enda!</h4>
+		<p>Trykk på den det blå krysset i høyre hjørne for å registrere- og planlegge en telefonsamtale</p>
+	</div>
+
+<!--Knapp for å videresende til "timer"-skjemaet-->
+	<button id="addInfo">+</button>
+</main>
+
+
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+
+	:global(body) {
+		background: linear-gradient(#117893, white);
+		font-family: 'Roboto', sans-serif;
+	}
+
+	(main) {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	(h3) {
+		color: #117893;
+	}
+
+	(h1) {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	(#intro) {
+		color: #117893;
+		margin: 150px 0px 0px 0px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
